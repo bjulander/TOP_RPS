365f899704a9ffed8cd78b03a92304b9f3c20a07
Blake Julander <blakejulander@gmail.com>
2023-10-06 06:51:44 -0700

    Now had computer and user selection working for game

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 
 let compArray = ["rock", "paper", "scissors"]
+let playerSelection = prompt("Rock, Paper, or Scissors?")
+
 
 function getComputerChoice(){
-    const randomElement = array[Math.floor(Math.random() * array.length)]
+    let compChoice = compArray[Math.floor(Math.random() * compArray.length)]
+    return compChoice
 }
+
+let compSelection = getComputerChoice()
+
+
+function playRound(playerSelection, compSelection){
+
+}
+
